bf802e0ecf036b53cf49c3ef7f47fe03b083aad6
LeandroDolensi <leandro.dolensi@aluno.ifsp.edu.br>
2025-11-17 13:55:28 -0300

    test: add suite jest settings setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const { createCjsPreset } = require('jest-preset-angular/presets');
+
+module.exports = {
+  ...createCjsPreset(),
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+};
